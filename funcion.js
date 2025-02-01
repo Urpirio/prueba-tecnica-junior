75372fcd09f6c7bf1b3e0ad8a4f056891c284dfe
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-02-01 13:59:06 -0400

    update

---
+++ funcion.js
@@ -0,0 +1,15 @@
+function alerta(){
+    alert("Acabas de precionar el boton")
+}
+
+function Nombre_c(){
+    let nombre = "holas";
+let apellido = "como estas?";
+r = nombre+ apellido;
+let N = document.querySelector("#span");
+ return N.innerHTML = r;
+}
+function hola(){
+    document.write("<h1>todo estar bien en esta prueba??</h1>")
+}
+
